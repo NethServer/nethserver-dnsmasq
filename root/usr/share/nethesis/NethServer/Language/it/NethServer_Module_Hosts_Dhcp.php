ccc203c103fa17961ee6ba145b5b58a402da8560
Davide Principi <davide.principi@nethesis.it>
2012-05-23 16:31:56 +0200

    dhcp reservation validator: fixed localizable messages in platform validator. Refs #1058

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Hosts_Dhcp.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Hosts_Dhcp.php
@@ -4,3 +4,7 @@ $L['Dhcp_Title'] = 'DHCP';
 $L['Dhcp_Configure_header'] = 'Assegnamento automatico degli indirizzi IP';
 $L['DhcpRangeStart_label'] = 'Inizio';
 $L['DhcpRangeEnd_label'] = 'Fine';
+
+$L['valid_platform,dhcp-reservation,ip-not-reserved-local,3'] = 'Già assegnato';
+$L['valid_platform,dhcp-reservation,ip-not-reserved-local,4'] = 'Non è nelle reti locali';
+$L['valid_platform,dhcp-reservation,ip-mac-address,1'] = 'In uso su un\'altra macchina';
