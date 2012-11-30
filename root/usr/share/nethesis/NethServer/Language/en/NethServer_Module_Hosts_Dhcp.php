e1feba6713d2945b20252a32ea6f4bc89f833b58
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-30 09:13:02 +0100

    Translation: refactor english and italian translation files #523

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Hosts_Dhcp.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Hosts_Dhcp.php
@@ -1,10 +1,9 @@
-<?php
+<?php 
 
-$L['Dhcp_Title'] = 'DHCP';
-$L['Dhcp_Configure_header'] = 'Automatic IP address assignment';
-$L['DhcpRangeStart_label'] = 'Begin';
-$L['DhcpRangeEnd_label'] = 'End';
-
-$L['valid_platform,dhcp-reservation,ip-not-reserved-local,3'] = 'Already assigned';
-$L['valid_platform,dhcp-reservation,ip-not-reserved-local,4'] = 'Not in local networks';
-$L['valid_platform,dhcp-reservation,ip-mac-address,1'] = 'Conflicts with another machine';
+$L['DhcpRangeEnd_label'] = 'End'; 
+$L['DhcpRangeStart_label'] = 'Begin'; 
+$L['Dhcp_Configure_header'] = 'Automatic IP address assignment'; 
+$L['Dhcp_Title'] = 'DHCP'; 
+$L['valid_platform,dhcp-reservation,ip-mac-address,1'] = 'Conflicts with another machine'; 
+$L['valid_platform,dhcp-reservation,ip-not-reserved-local,3'] = 'Already assigned'; 
+$L['valid_platform,dhcp-reservation,ip-not-reserved-local,4'] = 'Not in local networks'; 
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Hosts_Dhcp.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Hosts_Dhcp.php
@@ -1,10 +1,9 @@
-<?php
+<?php 
 
-$L['Dhcp_Title'] = 'DHCP';
-$L['Dhcp_Configure_header'] = 'Assegnamento automatico degli indirizzi IP';
-$L['DhcpRangeStart_label'] = 'Inizio';
-$L['DhcpRangeEnd_label'] = 'Fine';
-
-$L['valid_platform,dhcp-reservation,ip-not-reserved-local,3'] = 'Già assegnato';
-$L['valid_platform,dhcp-reservation,ip-not-reserved-local,4'] = 'Non è nelle reti locali';
-$L['valid_platform,dhcp-reservation,ip-mac-address,1'] = 'In uso su un\'altra macchina';
+$L['DhcpRangeEnd_label'] = 'Fine'; 
+$L['DhcpRangeStart_label'] = 'Inizio'; 
+$L['Dhcp_Configure_header'] = 'Assegnamento automatico degli indirizzi IP'; 
+$L['Dhcp_Title'] = 'DHCP'; 
+$L['valid_platform,dhcp-reservation,ip-mac-address,1'] = 'In uso su un\'altra macchina'; 
+$L['valid_platform,dhcp-reservation,ip-not-reserved-local,3'] = 'Già assegnato'; 
+$L['valid_platform,dhcp-reservation,ip-not-reserved-local,4'] = 'Non è nelle reti locali'; 
