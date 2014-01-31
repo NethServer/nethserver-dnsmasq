8f64a4eacd38e5c8798ea5561b8d67faf1a5be77
Davide Principi <davide.principi@nethesis.it>
2014-01-31 16:40:23 +0100

    Show text label in Hosts > DHCP > Configure form. Refs #2642

--- root/usr/share/nethesis/NethServer/Template/Hosts/Dhcp/Configure.php
+++ root/usr/share/nethesis/NethServer/Template/Hosts/Dhcp/Configure.php
@@ -1,6 +1,6 @@
 <?php
 
-echo $view->header('network')->setAttribute('template', $T('Dhcp_Configure_header'));
+echo $view->header()->setAttribute('template', $T('Dhcp_Configure_header'));
 
 echo $view->radioButton('DhcpStatus', 'disabled')->setAttribute('label', $T('status_disabled_label'));
 
