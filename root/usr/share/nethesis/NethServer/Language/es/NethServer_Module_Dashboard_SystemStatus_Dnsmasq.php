1f5ec74ef12b8b14ab7f85dc9c666f8538086166
Victor Moreno <victor_moreno_rubio@hotmail.com>
2014-05-05 16:02:01 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Dashboard_SystemStatus_Dnsmasq.php
@@ -0,0 +1,12 @@
+<?php 
+
+/* NethServer_Module_Dashboard_SystemStatus_Dnsmasq translation, language: es */
+
+$L['dns_dhcp_title'] = 'DNS y DHCP';
+$L['nameservers_label'] = 'DNS remoto';
+$L['dns_status_label'] = 'Servidor DNS ';
+$L['dhcp_status_label'] = 'Servidor DHCP';
+$L['dhcp_start_label'] = 'Inicio del rango IP';
+$L['dhcp_end_label'] = 'Final del rango IP';
+$L['enabled'] = 'Activado';
+$L['disabled'] = 'Desactivado';
